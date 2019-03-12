ed79bd5ee5f89eacb6f5666f8b179f55dbd11869
David Branagh-Miscampbell <dave@stormideas.com>
2019-03-12 00:46:57 +0000

    Re-adding public methods

--- src/Storm.GoogleAnalytics.Reporting/IGoogleAnalyticsService.cs
+++ src/Storm.GoogleAnalytics.Reporting/IGoogleAnalyticsService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Storm.GoogleAnalytics.Reporting.Configuration;
 using Storm.GoogleAnalytics.Reporting.Core;
 
@@ -12,5 +13,23 @@ namespace Storm.GoogleAnalytics.Reporting
         /// <param name="configurer">configures the request with which to query the Google Analytics Reporting API</param>
         /// <returns>Google Analytics Response</returns>
         IGoogleAnalyticsResponse Query(Func<IGoogleAnalyticsRequestConfigurerProfileId, IGoogleAnalyticsRequestConfigurerProfileId> configurer);
+        /// <summary>	
+        /// Query the Google Analytics Reporting API using the given request configuration	
+        /// </summary>	
+        /// <param name="requestConfig">the request with which to query the Google Analytics Reporting API</param>	
+        /// <returns>Google Analytics Response</returns>	
+        IGoogleAnalyticsResponse Query(IGoogleAnalyticsRequestConfiguration requestConfig);
+        /// <summary>	
+        /// Query the Google Analytics Reporting API using the given request configuration	
+        /// </summary>	
+        /// <param name="configurer">configures the request with which to query the Google Analytics Reporting API</param>	
+        /// <returns>Google Analytics Response</returns>	
+        Task<IGoogleAnalyticsResponse> QueryAsync(Func<IGoogleAnalyticsRequestConfigurerProfileId, IGoogleAnalyticsRequestConfigurerProfileId> configurer);
+        /// <summary>	
+        /// Query the Google Analytics Reporting API using the given request configuration	
+        /// </summary>	
+        /// <param name="requestConfig">the request with which to query the Google Analytics Reporting API</param>	
+        /// <returns>Google Analytics Response</returns>	
+        Task<IGoogleAnalyticsResponse> QueryAsync(IGoogleAnalyticsRequestConfiguration requestConfig);
     }
 }
